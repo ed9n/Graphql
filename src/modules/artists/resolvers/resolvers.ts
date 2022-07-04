8e47e501e384dbb88c928ade99dc22e3fa737803
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-07-04 19:29:15 +0300

    feat: add bandsApi

--- src/modules/artists/resolvers/resolvers.ts
+++ src/modules/artists/resolvers/resolvers.ts
@@ -1,4 +1,4 @@
-import { Artist, ObjectArtists } from '../interface/interface.artist'
+import { Artist, ObjectArtists } from '../interface/interface.artist';
 
 export const resArtists = {
 
@@ -8,19 +8,36 @@ export const resArtists = {
       return dataSources.artistsApi.getArtists(limit, offset)
         .then((value: ObjectArtists) => {
           const newObject = value.items.map((obj) => {
-            return { id: obj._id, ...obj, instruments: obj.instruments.join(', ') }
-          })
-          return newObject
-        })
+            
+            const id = obj.bandsIds.toString();
+            const response = dataSources.bandsApi.getBandById(id);
+
+            const arr = [];
+            arr.push(response);
+
+              return {
+                id: obj._id,
+              ...obj,
+                instruments: obj.instruments.join(', '),
+                bands: id ? arr.map((el) => {
+                  return el.then((value) => {
+                    return { id: value._id, ...value };
+                  });
+                }) : []
+              };
+          });
+          return newObject;
+
+        });
     },
 
-    artist: (_, { id }, { dataSources }) => {
+    artist: async (_, { id }, { dataSources }) => {
       return dataSources.artistsApi.getArtistById(id)
         .then((value: Artist) => {
-          const newObject = { id: value._id, ...value, instruments: value.instruments.join(', ') }
-          return newObject
-        })
+          const newObject = { id: value._id, ...value, instruments: value.instruments.join(', ') };
+          return newObject;
+        });
     }
   }
 
-}
+};
