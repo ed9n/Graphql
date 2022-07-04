b4646578068feda2de875462064b0ce7a9f1ed11
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-07-04 19:26:10 +0300

    refactor: apply eslint

--- src/modules/artists/datasources/artists-api.ts
+++ src/modules/artists/datasources/artists-api.ts
@@ -1,25 +1,25 @@
-import { RESTDataSource } from 'apollo-datasource-rest'
+import { RESTDataSource } from 'apollo-datasource-rest';
 
 export class ArtistsApi extends RESTDataSource {
   constructor () {
-    super()
-    this.baseURL = 'http://localhost:3002/v1/'
+    super();
+    this.baseURL = 'http://localhost:3002/v1/';
   }
 
   async getArtists (limit: string, offset: string) {
     if (limit && offset) {
-      return this.get(`artists?offset=${offset}&limit=${limit}`)
+      return this.get(`artists?offset=${offset}&limit=${limit}`);
     }
     if (limit) {
-      return this.get(`artists?limit=${limit}`)
+      return this.get(`artists?limit=${limit}`);
     }
     if (offset) {
-      return this.get(`artists?offset=${offset}`)
+      return this.get(`artists?offset=${offset}`);
     }
-    return this.get('artists')
+    return this.get('artists');
   }
 
   async getArtistById (id: string) {
-    return this.get(`artists/${id}`)
+    return this.get(`artists/${id}`);
   }
 }
